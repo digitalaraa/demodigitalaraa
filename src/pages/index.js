f4dce0cb6b50888c2ee4f35537b577890ebb03fc
sarandevarjun <saransri13@gmail.com>
2024-09-27 12:41:37 +0530

    layout case sensitive file changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Layout from "../components/Layout"; // Adjust path as necessary
+import Layout from "../components/layout";
 
 const IndexPage = () => {
   return (
